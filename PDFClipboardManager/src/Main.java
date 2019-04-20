e319053cd781b885bf30f33b354505de47491fb5
derMacon <silash169@gmail.com>
2019-04-20 23:30:51 +0200

    Created idea project, rescaling images working

---
+++ PDFClipboardManager/src/Main.java
@@ -0,0 +1,25 @@
+import java.io.File;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+        showNames("res/");
+        Boolean status = ImageResizer.resizeImage("res/marvinLarge.png", "res/marvinSmall.png",100,100);
+    }
+
+    private static void showNames(String path) {
+        File folder = new File(path);
+        File[] listOfFiles = folder.listFiles();
+
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isFile()) {
+                System.out.println("File " + listOfFiles[i].getName());
+            } else if (listOfFiles[i].isDirectory()) {
+                System.out.println("Directory " + listOfFiles[i].getName());
+            }
+        }
+    }
+}
+
