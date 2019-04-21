d8b3bdcf603a5ede5cb21967dbf1ace5e29b8c1e
derMacon <silash169@gmail.com>
2019-04-22 01:31:28 +0200

    Finally works with system wide hook

--- demos/KeyListener/src/nativeHook/GerTutCode.java
+++ demos/KeyListener/src/nativeHook/GerTutCode.java
@@ -1,5 +1,56 @@
 package nativeHook;
 
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.NativeHookException;
+import org.jnativehook.keyboard.NativeKeyEvent;
+import org.jnativehook.keyboard.NativeKeyListener;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Tut irgendwie nix :(
+ */
 public class GerTutCode {
-    
+    private static boolean running = true;
+
+    public static void main(String[] args) throws NativeHookException {
+        Logger l = Logger.getLogger(GlobalScreen.class.getPackage().getName());
+        l.setLevel(Level.OFF);
+
+        GlobalScreen.registerNativeHook();
+        GlobalScreen.addNativeKeyListener(listener);
+
+        while(running) {
+            try {
+                System.out.println("bis hier");
+                Thread.sleep(300);
+            } catch(InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        GlobalScreen.unregisterNativeHook();
+    }
+
+    private static NativeKeyListener listener = new NativeKeyListener() {
+        @Override
+        public void nativeKeyTyped(NativeKeyEvent e) {
+            System.out.println("Key pressed: "+ NativeKeyEvent.getKeyText(e.getKeyCode()));
+            if (e.getKeyCode() == NativeKeyEvent.VC_ESCAPE) {
+                running = false;
+            }
+        }
+
+        @Override
+        public void nativeKeyPressed(NativeKeyEvent nativeKeyEvent) {
+
+        }
+
+        @Override
+        public void nativeKeyReleased(NativeKeyEvent nativeKeyEvent) {
+
+        }
+    };
+
 }
--- demos/KeyListener/src/nativeHook/MyListener.java
+++ demos/KeyListener/src/nativeHook/MyListener.java
@@ -1,4 +1,23 @@
 package nativeHook;
 
-public class MyListener {
+import org.jnativehook.keyboard.NativeKeyEvent;
+import org.jnativehook.keyboard.NativeKeyListener;
+
+public class MyListener implements NativeKeyListener {
+    @Override
+    public void nativeKeyTyped(NativeKeyEvent nativeKeyEvent) {
+        System.out.println(nativeKeyEvent.getKeyChar());
+        System.out.println("works");
+    }
+
+    @Override
+    public void nativeKeyPressed(NativeKeyEvent nativeKeyEvent) {
+        System.out.println(nativeKeyEvent.getKeyChar());
+        System.out.println("workss");
+    }
+
+    @Override
+    public void nativeKeyReleased(NativeKeyEvent nativeKeyEvent) {
+        System.out.println("worksss");
+    }
 }
