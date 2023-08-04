d74bcae17194b662333d24ca9cdb625c7d8e43dd
hoffmann <silas.hoffmann@stud.uni-goettingen.de>
2023-08-05 01:14:04 +0200

    cleanup

--- img-viewer/src/main/java/com/dermacon/app/fileio/CSVException.java
+++
@@ -1,9 +0,0 @@
-package com.dermacon.app.fileio;
-
-public class CSVException extends Exception {
-
-    public CSVException(String message) {
-        super(message);
-    }
-
-}
--- img-viewer/src/main/java/com/dermacon/app/logik/InvalidInputException.java
+++
@@ -1,5 +0,0 @@
-package com.dermacon.app.logik;
-
-public class InvalidInputException extends Exception {
-
-}
