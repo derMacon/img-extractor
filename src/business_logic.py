45be0393e132bd3f3da6f01cc3334dedd0b11378
hoffmann <silas.hoffmann@stud.uni-goettingen.de>
2023-09-10 14:02:51 +0200

    minor cleanup

--- src/business_logic.py
+++
@@ -1,6 +0,0 @@
-# business_logic.py
-
-class BusinessLogic:
-    def do_something(self):
-        # Your business logic here
-        return "Result of the business logic"
--- src/logic/mock_service.py
+++
@@ -1,8 +0,0 @@
-class MockService:
-
-    def __init__(self):
-        self.counter = 0
-
-    def print_self(self):
-        self.counter += 1
-        print(self.counter)
