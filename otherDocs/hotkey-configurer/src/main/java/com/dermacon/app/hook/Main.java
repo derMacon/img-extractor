e2914915265533e3a2d4ef05ca47b2b7f7363055
hoffmann <silash169@gmail.com>
2023-10-12 23:49:58 +0200

    cleanup other docs

--- otherDocs/hotkey-configurer/src/main/java/com/dermacon/app/hook/Main.java
+++
@@ -1,9 +0,0 @@
-package com.dermacon.app.hook;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("starting");
-        Thread runner = new Thread(new HookRunner());
-        runner.start();
-    }
-}
